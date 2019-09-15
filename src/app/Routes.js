0ec1201864437c19e79a86aa32f4936e56fece87
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-14 17:33:44 -0700

    sets up Routes

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,2 +1,14 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import HomePage from "../homePage/HomePage";
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Redirect to="/" />
+    </Switch>
+  );
+}
+
+export default Routes;
