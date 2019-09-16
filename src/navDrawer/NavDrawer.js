9eecd891f8182558871562f67995aaecae276d4e
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-16 12:55:08 -0700

    recaftor ListItemLink

--- src/navDrawer/NavDrawer.js
+++ src/navDrawer/NavDrawer.js
@@ -4,32 +4,43 @@ import {
   Divider,
   List,
   ListItem,
-  ListItemText
+  ListItemText,
+  IconButton
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { NavLink } from "react-router-dom";
 import ProfileAvatar from "./profileAvatar";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
 function NavDrawer({ isDrawerOpen, setDrawerOpen }) {
   //generating classes
   const classes = useStyles();
   return (
-    <SwipeableDrawer 
+    <SwipeableDrawer
       className={classes.drawer}
       classes={{
         paper: classes.drawerPaper
       }}
       anchor="left"
       open={isDrawerOpen}
-      onClose={()=> setDrawerOpen(false)}
-      onOpen ={() => setDrawerOpen(true)}
+      onClose={() => setDrawerOpen(false)}
+      onOpen={() => setDrawerOpen(true)}
     >
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={() => setDrawerOpen(false)}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </div>
       <ProfileAvatar />
       <Divider />
       <List>
-        <ListItemLink to="/home" title="Home" />
-        <ListItemLink to="/projects" title="Projects" />
-        <ListItemLink to="/about" title="About" />
+        <ListItemLink to="/home" title="Home" className={classes.LinkItems} />
+        <ListItemLink
+          to="/projects"
+          title="Projects"
+          className={classes.LinkItems}
+        />
+        <ListItemLink to="/about" title="About" className={classes.LinkItems} />
       </List>
     </SwipeableDrawer>
   );
@@ -45,15 +56,33 @@ const useStyles = makeStyles(theme => ({
   },
   drawerPaper: {
     width: 340
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  LinkItems: {
+    textAlign: "center"
   }
 }));
 
-function ListItemLink({ title, to }) {
+function ListItemLink({ title, to, className }) {
   return (
-    <NavLink exact to={to} style={{ textDecoration: "none", color: "black" }}>
-      <ListItem button>
-        <ListItemText primary={title} />
-      </ListItem>
-    </NavLink>
+    <ListItem
+      component={AdapterLink}
+      exact
+      to={to}
+      button
+      className={className}
+    >
+      <ListItemText primary={title} />
+    </ListItem>
   );
 }
+
+const AdapterLink = React.forwardRef((props, ref) => (
+  <NavLink innerRef={ref} {...props} />
+));
