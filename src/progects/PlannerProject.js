632229ee7658b26650d828b01f600be57741efc7
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-16 20:48:29 -0700

    adds responsivness and Typography to plannerProject

--- src/progects/PlannerProject.js
+++ src/progects/PlannerProject.js
@@ -4,7 +4,8 @@ import {
   ExpansionPanel,
   ExpansionPanelSummary,
   Typography,
-  Grow
+  Grow,
+  useMediaQuery
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import VisibilitySensor from "react-visibility-sensor";
@@ -13,7 +14,6 @@ import iphoneExpenses from "../assets/iphone-add.png";
 
 function PlannerProject() {
   const classes = useStyles();
-
   return (
     <React.Fragment>
       <Paper className={classes.paper}>
@@ -43,6 +43,22 @@ function PlannerProject() {
               >
                 <Typography>What Worked</Typography>
               </ExpansionPanelSummary>
+              <div className={classes.expansionText}>
+                <ul>
+                  <Typography component="li">
+                    I found the expense tiles to be very easy and
+                    straightforward to use, of course, the available categories
+                    were designed around me, but I could never make myself keep
+                    a record of anything at all until this app.
+                  </Typography>
+                  <br />
+                  <Typography component="li">
+                    I think the Cards turned out to be a good design and
+                    engineering choice, they were pretty easy to refactor and
+                    add features too as they covered more and more components.
+                  </Typography>
+                </ul>
+              </div>
             </ExpansionPanel>
             <ExpansionPanel className={classes.expansionPanel}>
               <ExpansionPanelSummary
@@ -74,18 +90,19 @@ function PlannerProject() {
 }
 
 const useStyles = makeStyles(theme => ({
-  paper: { display: "flex", flexWrap: "wrap" },
+  paper: { display: "flex", flexWrap: "wrap", justifyContent: "space-evenly" },
   projectImg: {
     height: 700,
-    width: "33%",
+    width: 330,
     objectFit: "cover"
   },
   header: {
     textAlign: "center",
     margin: 30
   },
-  expansionPanelParent: { width: "66%" },
-  expansionSummery: {}
+  expansionPanelParent: { width: 660 },
+  expansionSummery: {},
+  expansionText: { margin: 20 }
 }));
 
 export default PlannerProject;
