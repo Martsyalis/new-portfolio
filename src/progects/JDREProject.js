3bdfc1d61630ac8896621b9b5f8d63b5f5237580
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-19 12:02:22 -0700

    changes JDRE text

--- src/progects/JDREProject.js
+++ src/progects/JDREProject.js
@@ -25,40 +25,20 @@ function JDREProject({ classes, isInView }) {
           <ListExpansionPanel
             classes={classes}
             title="What Worked"
-            p1="Redux was a great choice for this app. The app had alot of functionality dependant on a common large set of data and multiple layers of information availible to users based on their role. Use of Reducers and Dictioneries helped us keep the loading times relatively short, and protect the store from bugs in individual components. This was especially crusial since the project didn't have any tests to speak of, but more on that in 'What did not work' section"
-            p2="We opted for using service worker for notifications, Which worked great and were very easy to set up. Another advantage of this was that we were able to re-use the same logic for administrative website, so The Real Estate Agents were able to get dynamic notifications about new client or new chat message on their phone without having to build and publish a native app"
+            p1="Redux was a great choice for this app. The app had a lot of functionality dependant on a common, large set of data. And different layers of information were available to users depending on their role. Use of Reducers and Dictionaries helped us keep the loading times relatively short, and protect the store from bugs in individual components. This was especially crucial since the project didn't have a comprehensive suite of tests."
+            p2="We opted for using a service worker for notifications, which worked great and were very easy to set up. Another advantage of this was that we were able to re-use the same logic for the administrative website, so the real estate agents were able to get dynamic notifications about a new client or new chat message on their phone without us having to build and publish a native app"
           />
           <ListExpansionPanel
             classes={classes}
             title="What Didn't Work"
-            p1="Given a very ambitious initial time-frame and ever changing product goals and features we elected not to spend time developing a proper testing suite. This made initial development rapid and agile. But once the project evolved into a 2 complicated, feature rich front-ends and 2 complicated and computationaly demaning back-ends, adding one ferature as often as not meant breaking another. Things got so bad that by the end of the development we frequently had to rely on our clients to tell us when something in one of the apps broke. The complex and massive Database and constreins of App store meant that it would frequently take us a better part of a week to release a fix for product breaking bug"
-            p2="PWA aspect of the app certainly makes it much easier to use on
-              a daily bases and this app simply wouldn't be usable without
-              it, but My cashing technique is inefficient and probably
-              buggy. The only diagnostic tool that I have for PWA, which is
-              google's Lighthouse, shows near 100% ratings. But personally,
-              Everytime the app is updated and the new version has to be
-              loaded, the cashing takes a good 6-7 seconds which feels
-              excruciatingly slow. Half the time I close down the app
-              entirely and re-open it out of impatience. The solution might
-              be to simply let user know whats happening by displaying
-              'Updating' Messege. But Im a little worried about messing with
-              Service Worker any more then I already have."
+            p1="Given a very ambitious initial time-frame and ever-changing product goals and features we elected not to spend time developing a proper testing suite. This made initial development rapid and agile. But once the project evolved into a 2 complicated, feature-rich front ends and 2 complicated and computationally demanding back ends, adding one feature as often as not meant breaking another. Things got so bad that by the end of the development we frequently had to rely on our clients to tell us when something in one of the apps broke. The complex and massive Database and constraints of App Store meant that it would frequently take us a better part of a week to release a fix for an experience breaking bug"
+            p2="Since no one on our team has built a mobile app before we opted for using Apache Cordova to make a hybrid app. Cordova takes a webpage build with HTML/JS and wraps it in a browser app that displays only that page. Developers can then publish this one-page browser on App Stores. This looked like a great solution at first but soon proved to be excessively buggy and unpredictable. The documentation was almost non-existent. We had to rely on anecdotal, hacky solutions found on lesser-known message boards. When it worked, It was a great, but unpredictable errors at build time meant that we couldn't accurately predict time frames of new releases. Most of all, It was very frustrating and discouraging to work with. While we persevered and made it work, React Native would probably be a better and safer choice."
           />
           <ListExpansionPanel
             classes={classes}
-            title="What needs to be added"
-            p1="In future versions, the tiles need to be customizable to an
-              individual user. the difficulty with that right now is that
-              I can't think of the categories that others might need, And
-              I can't offer user to name their own because icons are so
-              crucial to the 'effortlessness' of adding a new expense"
-            p2="More Data is needed for this, but where the Charts could
-              really shine is in a multi-month analysis. Until then I have
-              no long term trends to analyze. Still, it would be nice to
-              come up with more data to show in Charts, if only to
-              practice Data Visualisation which was one of the original
-              goals of this project"
+            title="What Could Be Added"
+            p1=""
+            p2=""
           />
         </div>
       </Slide>
