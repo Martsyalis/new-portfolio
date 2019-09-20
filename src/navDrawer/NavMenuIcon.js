69be4796ddb61f8bfeafc60a571ff4a839e90e96
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-20 13:36:50 -0700

    remove BannerBar

---
+++ src/navDrawer/NavMenuIcon.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Toolbar, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+function NavMenuIcon({ setDrawerOpen }) {
+  const classes = useStyles();
+  return (
+    <Toolbar>
+      <IconButton
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+        onClick={() => setDrawerOpen(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+    </Toolbar>
+  );
+}
+
+const useStyles = makeStyles(theme => ({}));
+
+export default NavMenuIcon;
