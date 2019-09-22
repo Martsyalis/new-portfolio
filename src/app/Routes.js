b0cba88c57462ce65693afd7fa0d020538b1bb04
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-21 18:15:06 -0700

    changes default url to projects

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -7,11 +7,11 @@ import About from "../about/About";
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/" component={Projects} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/about" component={About} />
 
-      <Redirect to="/projects" />
+      <Redirect to="/" />
     </Switch>
   );
 }
