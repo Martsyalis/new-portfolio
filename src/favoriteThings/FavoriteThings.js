73d3c518c870175d405dc7172a04fb87ac60ffb0
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-23 18:38:57 -0700

    both walls in place, cards still scewd

--- src/favoriteThings/FavoriteThings.js
+++ src/favoriteThings/FavoriteThings.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./favoriteThings.css";
 import { Card, CardContent, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 function FavoriteThings() {
   return (
     <div className="home-page">
-      <div className="left-wall">
+      <div className="wall left-wall">
         <div className="left-window">
           <QuoteCard
             text="If only there were evil people somewhere insidiously committing evil deeds, and it were necessary only to separate them from the rest of us and destroy them. But the line dividing good and evil cuts through the heart of every human being."
@@ -24,28 +26,14 @@ function FavoriteThings() {
           />
         </div>
       </div>
-      <div className="center-wall">
-        <div className="center-window">
+      <div className="wall right-wall">
+        <div className="right-window">
           <QuoteCard
             text="To look at the symbolic dimensions of social action- art, religion, ideology, science, law, morality, common sense is not to turn away from the existential dilemmas of life for some empyrean realm of de-emotionalized forms; it is to plunge into the midst of them. The essential vocation of interpretive anthropology is not to answer our deepest questions, but to make available to us answers that others, guarding other sheep in other valleys, have given, and thus to include them in the consultable record of what man has said."
             attribution="Clifford Geertz"
           />
         </div>
-        <div className="center-window">
-          <QuoteCard
-            text="To look at the symbolic dimensions of social action- art, religion, ideology, science, law, morality, common sense is not to turn away from the existential dilemmas of life for some empyrean realm of de-emotionalized forms; it is to plunge into the midst of them. The essential vocation of interpretive anthropology is not to answer our deepest questions, but to make available to us answers that others, guarding other sheep in other valleys, have given, and thus to include them in the consultable record of what man has said."
-            attribution="Clifford Geertz"
-          />
-        </div>
-      </div>
-      <div className="right-wall">
-        <div className="window2">
-          <QuoteCard
-            text="To look at the symbolic dimensions of social action- art, religion, ideology, science, law, morality, common sense is not to turn away from the existential dilemmas of life for some empyrean realm of de-emotionalized forms; it is to plunge into the midst of them. The essential vocation of interpretive anthropology is not to answer our deepest questions, but to make available to us answers that others, guarding other sheep in other valleys, have given, and thus to include them in the consultable record of what man has said."
-            attribution="Clifford Geertz"
-          />
-        </div>
-        <div className="window2">
+        <div className="right-window">
           <QuoteCard
             text="To look at the symbolic dimensions of social action- art, religion, ideology, science, law, morality, common sense is not to turn away from the existential dilemmas of life for some empyrean realm of de-emotionalized forms; it is to plunge into the midst of them. The essential vocation of interpretive anthropology is not to answer our deepest questions, but to make available to us answers that others, guarding other sheep in other valleys, have given, and thus to include them in the consultable record of what man has said."
             attribution="Clifford Geertz"
@@ -57,8 +45,10 @@ function FavoriteThings() {
 }
 
 function QuoteCard({ title, text, attribution }) {
+  const classes = useStyles();
+
   return (
-    <Card>
+    <Card className={classes.Card}>
       <CardContent>
         <Typography variant="body1">"{text}"</Typography>
         <Typography variant="body1">-{attribution}</Typography>
@@ -66,5 +56,9 @@ function QuoteCard({ title, text, attribution }) {
     </Card>
   );
 }
+
+const useStyles = makeStyles(theme => ({
+  Card: { height: "100%" }
+}));
 
 export default FavoriteThings;
